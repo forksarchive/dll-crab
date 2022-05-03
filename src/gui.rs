05ccc311e4520d9f7a474a20454303f16a7aee48
aiocat <aiocat@protonmail.com>
2022-05-03 16:53:01 +0300

    update version

--- src/gui.rs
+++ src/gui.rs
@@ -158,7 +158,7 @@ impl eframe::App for DLLCrabWindow {
         egui::TopBottomPanel::bottom("bottom")
             .frame(main_frame)
             .show(ctx, |ui: &mut egui::Ui| {
-                ui.small("v1.1.0");
+                ui.small("v1.2.0");
                 egui::menu::bar(ui, |ui: &mut egui::Ui| {
                     ui.hyperlink_to("Source Code", "https://github.com/aiocat/dll-crab");
                     ui.hyperlink_to(
